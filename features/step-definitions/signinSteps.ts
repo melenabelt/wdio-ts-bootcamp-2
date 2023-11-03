afbe7b6999a2541045196aab843095ccc12a94c0
Elena Correa <melenabelt@gmail.com>
2023-11-03 13:52:22 -0500

    Some unnecesary code was deleted and require from cucumberOpts was modified

--- features/step-definitions/signinSteps.ts
+++ features/step-definitions/signinSteps.ts
@@ -1,22 +1,15 @@
 import { Given, When, Then, After } from '@wdio/cucumber-framework';
-import signInPage from '../../page-objects/signinPage';
-
+import SigninPage from '../../page-objects/signinPage';
 import { SigninTask } from '../tasks/signinTask';
 
 import {expect} from 'chai';
-import signinPage from '../../page-objects/signinPage';
 
 const signinTask = new SigninTask()
 
 let manualInterventionRequired = false
 
-After(async function () {
-    await signinTask.signinHeader.moveTo();
-    await signinTask.signoutLink.click();
-  });
-
 Given(/^The user enters the Amazon homepage$/, async () => {
-    await signInPage.open()
+    await SigninPage.open()
 })
 
 When(/^The user enters the correct email and password from header$/, async () => {
@@ -26,8 +19,8 @@ When(/^The user enters the correct email and password from header$/, async () =>
 
 When(/^The user re-enter password and captcha$/, async () => {
 
-    if (signinPage.captchaSection.isExisting()) {
-        await signInPage.passwordInput.setValue("helloelena1234")
+    if (SigninPage.captchaSection.isExisting()) {
+        await SigninPage.passwordInput.setValue("helloelena1234")
         manualInterventionRequired = true;
         await new Promise(resolve => setTimeout(resolve, 5000));
     }
@@ -36,6 +29,8 @@ When(/^The user re-enter password and captcha$/, async () => {
 Then(/^The user sees Sign Out link on Account & Lists$/, async () => {
     await signinTask.signinHeader.moveTo();
     await expect(signinTask.signoutLink).exist
+    await signinTask.signinHeader.moveTo();
+    await signinTask.signoutLink.click();
 })
 
 // Other scenarios
