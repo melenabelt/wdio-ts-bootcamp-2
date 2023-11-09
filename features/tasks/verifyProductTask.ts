c5e0ce59a17eabd41c9801e771e53d05500e076f
Elena Correa <melenabelt@gmail.com>
2023-11-08 19:04:37 -0500

    some exceptions were added

--- features/tasks/verifyProductTask.ts
+++ features/tasks/verifyProductTask.ts
@@ -27,24 +27,35 @@ export class VerifyProductTask extends VerifyProductPage {
   }
 
   public async getImportFees() {
-    const importFeesText = await this.importFees.getText();
-    const importFeesRegex: RegExpMatchArray | null =
-      importFeesText.match(/\d+\.\d+/);
+    let importFeesText = '';
+
+    try {
+        importFeesText = await this.importFees.getText();
+    } catch (error) {
+        console.log("El elemento importFees no se encontró.");
+    }
+
+    const importFeesRegex: RegExpMatchArray | null = importFeesText.match(/\d+\.\d+/);
     let numImportFees = 0.0;
 
     console.log("Import Fees Text:", importFeesText);
 
     if (importFeesRegex !== null) {
-      numImportFees = parseFloat(importFeesRegex[0]);
+        numImportFees = parseFloat(importFeesRegex[0]);
     }
 
     console.log("Import Fees Float", numImportFees);
 
     return numImportFees;
-  }
+}
 
   public async getDetailsFinalPrice() {
-    await this.detailsButton.click()
+    try {
+      await this.detailsButton.click()
+  } catch (error) {
+      console.log("Details button wasn't found");
+  }
+    
     const totalSpanText = await this.finalPrice.getText()
 
     console.log("Span total price Text:", totalSpanText)
